0cbb4b95a693639a6475d29e3c0686bf4a100e7a
Firas <f.sahli@quict.nl>
2021-02-26 23:28:40 +0100

    add header and footer composers
    Signed-off-by: Firas <f.sahli@quict.nl>

--- app/Http/View/Composers/HeaderFooterComposer.php
+++ app/Http/View/Composers/HeaderFooterComposer.php
@@ -3,6 +3,8 @@
 namespace App\Http\View\Composers;
 
 use App\Models\site_settings;
+use App\Models\Setting;
+use App\Models\Languages;
 use Illuminate\View\View;
 
 
@@ -19,10 +21,24 @@ class HeaderFooterComposer
     {
         // Dependencies automatically resolved by service container...
         // site_settings $site_settings
-        $lang_id = 2;
+        if( session()->has( 'locale' ) ){
+           $lang_in_session = session('locale');
+           $language = Languages::where('code', $lang_in_session)->first();
+           $lang_id = $language->id;
+        }
+        else {
+          $language = Languages::where('is_default', 'Y')->first();
+          $lang_id = $language->id;
+        }
+
         $setting_item = site_settings::where('language', $lang_id)->first();
 
+        $title_text_color = Setting::where('title', 'siteTitleColor')->first();
+
         $this->siteName = $setting_item->site_name;
+        $this->site_title = $setting_item->site_title;
+        $this->site_logo = $setting_item->site_logo;
+        $this->site_title_text_color = $title_text_color->value;
     }
 
     /**
@@ -33,6 +49,9 @@ class HeaderFooterComposer
      */
     public function compose(View $view)
     {
-        $view->with('site_name', $this->siteName);
+        $view->with('site_name', $this->siteName)
+             ->with('site_logo', $this->site_logo)
+             ->with('site_title', $this->site_title)
+             ->with('site_title_text_color', $this->site_title_text_color);
     }
 }
