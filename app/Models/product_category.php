33e61cbb54625698594803691a2fa8ae587ca464
Firas <f.sahli@quict.nl>
2021-03-13 23:05:26 +0100

    Add product_category model

---
+++ app/Models/product_category.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+use Kyslik\ColumnSortable\Sortable;
+use Illuminate\Database\Eloquent\SoftDeletes; //needed for soft delete
+
+class product_category extends Model
+{
+    use HasFactory;
+
+    use Sortable;
+    use SoftDeletes;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'title',
+        'desc',
+        'slug',
+        'parent_category',
+        'langauge',
+        'shw',
+        'deleted',
+        'deleted_at'
+    ];
+
+    public $sortable = ['id', 'title', 'langauge', 'shw', 'created_at', 'updated_at'];
+
+    /**
+     * The attributes that should be hidden for arrays.
+     *
+     * @var array
+     */
+    protected $hidden = [
+
+    ];
+
+    /**
+     * The attributes that should be cast to native types.
+     *
+     * @var array
+     */
+    protected $casts = [
+        'email_verified_at' => 'datetime',
+    ];
+
+    public function language(){
+      return $this->belongsTo(Languages::class);
+    }
+
+    
+}
